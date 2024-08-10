7b823dec98bf1a82e3675ab816ebdf8cd4e29e6e
hotsuma kawashima <hkawashimajob1992@gmail.com>
2024-08-10 19:01:56 +0900

    update logistic

--- logistic.py
+++ logistic.py
@@ -10,6 +10,7 @@ from sklearn.model_selection import StratifiedKFold
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import log_loss
 from sklearn.metrics import precision_recall_curve, average_precision_score
+from sklearn.metrics import roc_curve, auc, roc_auc_score
 
 data = pd.read_csv('creditcard.csv')
 
@@ -72,3 +73,20 @@ plt.xlim([0.0, 1.0])
 plt.title('Precision-Recall curve: Average Precision = {0:0.2f}'.format(
           average_precision))
 plt.savefig('precision_recall.png')
+
+fpr, tpr, thresholds = roc_curve(preds['trueLabel'],
+                                 preds['prediction'])
+
+areaUnderROC = auc(fpr, tpr)
+
+plt.figure()
+plt.plot(fpr, tpr, color='r', lw=2, label='ROC curve')
+plt.plot([0, 1], [0, 1], color='k', lw=2, linestyle='--')
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.05])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('Receiver operating characteristic: \
+          Area under the curve = {0:0.2f}'.format(areaUnderROC))
+plt.legend(loc="lower right")
+plt.savefig('roc.png')
